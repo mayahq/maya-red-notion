d4c046953f64dfaa27f53f16f885cd46037ce7e7
Dushyant Yadav <dushyant9309@gmail.com>
2022-04-06 17:50:39 +0530

    Use axios for update-db node

--- src/nodes/notionUpdateDb/notionUpdateDb.schema.js
+++ src/nodes/notionUpdateDb/notionUpdateDb.schema.js
@@ -3,7 +3,8 @@ const {
     Schema,
     fields
 } = require('@mayahq/module-sdk')
-const {getDatabaseId} = require("../../util")
+const makeRequestWithRefresh = require('../../util/reqWithRefresh')
+const { getDatabaseId } = require("../../util")
 
 class NotionUpdateDb extends Node {
     constructor(node, RED, opts) {
@@ -19,8 +20,8 @@ class NotionUpdateDb extends Node {
         category: 'Maya Red Notion',
         isConfig: false,
         fields: {
-            url: new fields.Typed({type: 'str', defaultVal: '', allowedTypes: ['msg', 'flow', 'global']}),
-            properties: new fields.Typed({type: 'json', defaultVal: '{}', allowedTypes: ['msg', 'flow', 'global']}),
+            url: new fields.Typed({ type: 'str', defaultVal: '', allowedTypes: ['msg', 'flow', 'global'] }),
+            properties: new fields.Typed({ type: 'json', defaultVal: '{}', allowedTypes: ['msg', 'flow', 'global'] }),
         },
 
     })
@@ -29,80 +30,33 @@ class NotionUpdateDb extends Node {
         // Do something on initialization of node
     }
 
-    async refreshTokens() {
-        const newTokens = await refresh(this)
-        await this.tokens.set(newTokens)
-        return newTokens
-    }
-
     async onMessage(msg, vals) {
-        console.log("vals", vals);
-        this.setStatus("PROGRESS", "updating notion database...");
-        var fetch = require("node-fetch"); // or fetch() is native in browsers
+        this.setStatus("PROGRESS", "Updating notion database...")
+
         let database_id = getDatabaseId(vals.url)
-        let config_body = {};
-        if(vals.properties && Object.keys(vals.properties).length > 0)	config_body["properties"] = vals.properties;
-        let fetchConfig = {
+        let configBody = {}
+
+        if (vals.properties && Object.keys(vals.properties).length > 0) configBody["properties"] = vals.properties
+        const request = {
             url: `https://api.notion.com/v1/databases/${database_id}`,
             method: "PATCH",
             headers: {
                 "Authorization": `Bearer ${this.tokens.vals.access_token}`,
-                "Content-Type": "application/json",
                 "Notion-Version": "2021-08-16"
             },
-            body: JSON.stringify({
-            	...config_body,
-            })
+            data: configBody
         }
-        try{
-            let res = await fetch(fetchConfig.url, 
-            {
-                method: fetchConfig.method,
-                headers: fetchConfig.headers,
-                body: fetchConfig.body
-            });
-            let json = await res.json();
-            console.log(json);
-            if(json.error){
-                if(json.error.code === 401){
-                    const { access_token } = await this.refreshTokens()
-                    if (!access_token) {
-                        this.setStatus('ERROR', 'Failed to refresh access token')
-                        msg["__isError"] = true;
-                        msg.error = {
-                            reason: 'TOKEN_REFRESH_FAILED',
-                        }
-                        return msg
-                    }
-                    fetchConfig.headers.Authorization = `Bearer ${access_token}`;
-                    res = await fetch(fetchConfig.url, 
-                            {
-                                method: fetchConfig.method,
-                                headers: fetchConfig.headers
-                            });
-                    json = await res.json();
-                    if(json.error){
-                        msg.error = json.error;
-                        this.setStatus("ERROR", json.error.message);
-                        return msg;
-                    }
-                } else {
-                    msg["__isError"] = true;
-                    msg.error = json.error;
-                    this.setStatus("ERROR", json.error.message);
-                    return msg;
-                }
-                
-            }
-            msg.payload = json;
-            this.setStatus("SUCCESS", "notion database updated");
-            return msg;
+        try {
+            const response = await makeRequestWithRefresh(this, request)
+            msg.payload = response.data
+            this.setStatus("SUCCESS", "Notion database updated")
+            return msg
         }
-        catch(err){
-            msg["__isError"] = true;
-            msg.error = err;
-            this.setStatus("ERROR", "error occurred");
-            return msg;
+        catch (err) {
+            msg["__isError"] = true
+            msg.error = err
+            this.setStatus("ERROR", "error occurred")
+            return msg
         }
 
     }
